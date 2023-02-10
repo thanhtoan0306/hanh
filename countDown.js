2b152fcb373801399a48d15108e907bf734f86f9
toan <thanhtoan0306@gmail.com>
2023-02-11 00:22:10 +0700

    countdown

---
+++ countDown.js
@@ -0,0 +1,26 @@
+var countDownDate = new Date("Feb 11, 2023 00:21:00").getTime();
+
+var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("demo").innerHTML = "Đếm ngược: " + days + "ngày " + hours + "giờ "
+        + minutes + "phút " + seconds + "giây ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "HAPPY BIRTHDAY HANH";
+    }
+}, 1000);
